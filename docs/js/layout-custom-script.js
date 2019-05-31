8b44a9a299a3cc2c9152be7f7abfe7826e0f5198
ada-unravel <45609785+ada-unravel@users.noreply.github.com>
2019-05-31 01:34:06 -0700

    Delete old build, upload api.json

--- docs/js/layout-custom-script.js
+++
@@ -1,9 +0,0 @@
-  var _eiq = _eiq || [];
-  var _engagio_settings = {
-    accountId: "5472413401247adb4ef0866e4acda62c86d5dd1e"
-  };
-  (function() {
-    var ei = document.createElement('script'); ei.type = 'text/javascript'; ei.async = true;
-    ei.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'web-analytics.engagio.com/js/ei.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ei, s);
-  })();
